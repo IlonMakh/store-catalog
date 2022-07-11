6857ed91ea62e61970de031ecead529c71e1fd8c
Ilona <mitsunaki@yandex.ru>
2022-07-11 15:36:01 +0300

    feat: add search form

---
+++ online-store/src/components/search/search.ts
@@ -0,0 +1,48 @@
+import './search.css';
+
+export class Search {
+    draw() {
+        const input: HTMLElement = document.createElement('input');
+        input.setAttribute('type', 'search');
+        input.setAttribute('autofocus', 'autofocus');
+        input.setAttribute('placeholder', 'Type something...');
+        input.classList.add('search_input');
+        (<HTMLElement>document.querySelector('.search')).append(input);
+    }
+
+    search() {
+        const input: HTMLInputElement = document.querySelector('.search_input') as HTMLInputElement;
+        input.addEventListener('input', function () {
+            const searchValue: string = input.value.trim().toLowerCase();
+            const searchingItems: NodeListOf<HTMLElement> = document.querySelectorAll('.catalog_item');
+
+            if (searchValue != '') {
+                searchingItems.forEach(function (elem: HTMLElement) {
+                    if (
+                        (<HTMLElement>elem.querySelector('.item_name')).innerText.toLowerCase().search(searchValue) ==
+                        -1
+                    ) {
+                        elem.classList.add('hide');
+                    } else {
+                        elem.classList.remove('hide');
+                    }
+                });
+            } else {
+                searchingItems.forEach(function (elem: HTMLElement) {
+                    elem.classList.remove('hide');
+                });
+            }
+
+            if (document.querySelectorAll('.hide').length == document.querySelectorAll('.catalog_item').length) {
+                if (!document.querySelector('.no-result-msg')) {
+                    const noResultMsg: HTMLElement = document.createElement('div');
+                    noResultMsg.classList.add('no-result-msg');
+                    noResultMsg.innerHTML = "Sorry, now we don't have the product you are looking for...";
+                    (<HTMLElement>document.querySelector('.catalog_wrapper')).append(noResultMsg);
+                }
+            } else {
+                (<HTMLElement>document.querySelector('.no-result-msg')).remove();
+            }
+        });
+    }
+}
--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,5 +1,10 @@
 import { Socks } from './components/catalog/catalogView';
+import { Search } from './components/search/search';
 import './index.css';
 
 const socksCatalog = new Socks();
 socksCatalog.draw();
+
+const searchInput = new Search();
+searchInput.draw();
+searchInput.search();
